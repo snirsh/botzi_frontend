97c941f7a59cefbd9c32eaa7befdfbc3959449ae
TsilaHamer <tsilah@vision.bi>
2020-06-12 01:17:42 +0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+
 import { NgModule } from '@angular/core';
 
+import { HomeSlidesModule } from './home-slides/home-slides.module';
+import { SignUpModule } from './sign-up/sign-up.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
+import { SignUpComponent } from './sign-up/components/sign-up/sign-up.component';
 
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'sign-up-organization', component: SignUpComponent},
+  {path: 'sign-up-volunteer', component: SignUpComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    NavMenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    HomeSlidesModule,
+    SignUpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
